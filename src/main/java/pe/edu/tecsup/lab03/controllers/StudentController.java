29c2c38ed0262acfa86b66cf032e96122e98b957
Melany Quispe <melany.ql.2006@gmail.com>
2026-09-04 20:29:20 -0500

    Creacion de esta clase

---
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -0,0 +1,10 @@
+package pe.edu.tecsup.lab03.controllers;
+
+public class StudentController {
+
+    public void saludar() {
+        System.out.println("¡Hola! Modificación realizada con éxito en la rama sprint-1.");
+    }
+
+}
+

375fe77d9b0e230797b126d535f508caf993f716
Melany Quispe <melany.ql.2006@gmail.com>
2026-09-04 20:56:19 -0500

    Modificación de StudentController en sprint-2

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -2,9 +2,14 @@ package pe.edu.tecsup.lab03.controllers;
 
 public class StudentController {
 
-    public void saludar() {
-        System.out.println("¡Hola! Modificación realizada con éxito en la rama sprint-1.");
+    public void mostrarMensaje() {
+        System.out.println("Que el brillo del sol ilumine tu tarde y te traiga mucha paz.");
+    }
+
+    public void mensajeSprint2() {
+        System.out.println("Actualización de Sprint 2 completada con éxito.");
     }
 
 }
+
 
